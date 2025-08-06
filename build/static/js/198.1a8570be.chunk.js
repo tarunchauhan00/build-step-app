01177e4a5ac4815792ebb1cb58558e9a3e91f095
tarunchauhan00 <135992406+tarunchauhan00@users.noreply.github.com>
2025-08-06 22:27:02 +0530

    Add files via upload

---
+++ build/static/js/198.1a8570be.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgiver_ui=self.webpackChunkgiver_ui||[]).push([[198],{198:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=198.1a8570be.chunk.js.map
